7ebb4c5ebd0e340a0b71740251a04c6f14156c9a
RemPsyc <RemPsyc@users.noreply.github.com>
2019-10-13 17:24:35 -0400

    Update randomizeFunction.R

--- randomizeFunction.R
+++ randomizeFunction.R
@@ -8,7 +8,7 @@ randomize <- function (design="between",Ncondition=3,n=9,condition.names=c("a","
       Condition <- rbind(Condition, t(t(x)), # Add new stats to dataframe
                          stringsAsFactors = FALSE)
     }
-  }
+    }
   if (design=="within") {
     Condition <- Condition
     for (i in 1:n){ # Generate the random values for n participants and Nconditions
@@ -16,7 +16,7 @@ randomize <- function (design="between",Ncondition=3,n=9,condition.names=c("a","
       Condition <- rbind(Condition, x, # Adds new conditions to dataframe row by row
                          stringsAsFactors = FALSE) # Not as factors as this can create problems
     }
-  }
+    }
   Condition <- mapvalues(as.matrix(Condition), 1:Ncondition, condition.names) # Converts numbers to condition labels
   for (i in 1:n) {
     Condition[i,1] <- paste(Condition[i,], collapse=" - ") # Adds hyphen between conditions for easier read
@@ -24,7 +24,7 @@ randomize <- function (design="between",Ncondition=3,n=9,condition.names=c("a","
   if(ncol(Condition) > 2) { Condition[,2:ncol(Condition)] <- NA}
   Condition <- as.data.frame(Condition)
   id <- t(t(1:n))
-  final_table <- data.frame(id, Condition, matrix(NA, ncol = length(col.names)))
+  final_table <- data.frame(id, Condition, matrix(NA, ncol = length(col.names)))[,1:length(col.names)]
   names(final_table) <- col.names
   final_table
 }
