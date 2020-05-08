2b563070730025117edbaec47ab8a64a1b858454
RemPsyc <RemPsyc@users.noreply.github.com>
2020-05-08 18:36:59 -0400

    Update niceViolinFunction.R

--- niceViolinFunction.R
+++ niceViolinFunction.R
@@ -1,4 +1,4 @@
-niceViolin <- function (Group,Response,boot=T,bootstraps=2000,Manual.Colour=F,what.Colours,has.ylabels=T,has.xlabels=T,Manual.xlabels=F,xlabels=NULL,has.xtitle=T,has.ytitle=T,ytitle=waiver(),xtitle=waiver(),compare=F,comp1=NULL,comp2=NULL,Manual.compare=F,signif_annotation=NULL,signif_yposition=NULL,signif_xmin=NULL,signif_xmax=NULL) {
+niceViolin <- function (Group,Response,boot=T,bootstraps=2000,what.Colours,xlabels=NULL,ytitle=waiver(),xtitle=waiver(),has.ylabels = T,has.xlabels = T,comp1=NULL,comp2=NULL,signif_annotation=NULL,signif_yposition=NULL,signif_xmin=NULL,signif_xmax=NULL) {
   Data <- data.frame(Group, Response)
   class(Data$Response) <- "numeric"
   if(!require(rcompanion)){install.packages("rcompanion") + library(rcompanion)}
@@ -19,16 +19,16 @@ niceViolin <- function (Group,Response,boot=T,bootstraps=2000,Manual.Colour=F,wh
                    y = Response,
                    fill = factor(Group))) + 
     theme_bw(base_size = 24) +
-    {if (Manual.Colour == TRUE) scale_fill_manual(values=what.Colours)} +
-    {if (Manual.xlabels == TRUE) scale_x_discrete(labels=c(xlabels))} +
+    {if (!missing(what.Colours)) scale_fill_manual(values=what.Colours)} +
+    {if (!missing(xlabels)) scale_x_discrete(labels=c(xlabels))} +
     ylab(ytitle) +
     xlab(xtitle) +
     geom_violin() +
-    geom_point(aes(y = dataSummary$Mean), 
+    geom_point(aes(y = Mean), 
                color = "black", 
                size = 4, 
                data = dataSummary) + 
-    geom_errorbar(aes(y = dataSummary$Mean, 
+    geom_errorbar(aes(y = Mean, 
                       ymin = dataSummary[,6], 
                       ymax = dataSummary[,7]),
                   color = "black", 
@@ -38,10 +38,10 @@ niceViolin <- function (Group,Response,boot=T,bootstraps=2000,Manual.Colour=F,wh
     theme(legend.position = "none", 
           axis.text.x = element_text(colour="black"), 
           axis.text.y = element_text(colour="black"),
-          axis.title.x= {if (has.xtitle == FALSE) element_blank()},
-          axis.title.y = switch((has.ytitle == FALSE) + 1,
-                                element_text(angle = 90), 
-                                element_blank()),
+          # axis.title.x= {if (has.xtitle == FALSE) element_blank()},
+          # axis.title.y = switch((has.ytitle == FALSE) + 1,
+          #                      element_text(angle = 90), 
+          #                      element_blank()),
           panel.grid.major=element_blank(),
           panel.grid.minor=element_blank(),
           panel.border=element_blank(),
@@ -51,11 +51,11 @@ niceViolin <- function (Group,Response,boot=T,bootstraps=2000,Manual.Colour=F,wh
                                      axis.ticks.y=element_blank())} +
     {if (has.xlabels == FALSE) theme(axis.text.x=element_blank(),
                                      axis.ticks.x=element_blank())} +
-    {if (compare == TRUE) geom_signif(comparisons = list(c(comp1, comp2)), 
+    {if (!missing(comp1)) geom_signif(comparisons = list(c(comp1, comp2)), 
                                       map_signif_level=TRUE, 
                                       size= 1.3, 
                                       textsize=8)} +
-    {if (Manual.compare == TRUE) geom_signif(annotation=signif_annotation, 
+    {if (!missing(signif_annotation)) geom_signif(annotation=signif_annotation, 
                                              y_position=signif_yposition, 
                                              xmin=signif_xmin, 
                                              xmax=signif_xmax,
