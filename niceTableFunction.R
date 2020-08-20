6b1cb7eb0f5acd0c04a1a873b9968f4896653eeb
RemPsyc <RemPsyc@users.noreply.github.com>
2020-08-20 17:22:56 -0400

    Update niceTableFunction.R

--- niceTableFunction.R
+++ niceTableFunction.R
@@ -1,4 +1,4 @@
-niceTable <- function (dataframe) {
+niceTable <- function (dataframe, special = FALSE) {
   if(!require(flextable)){install.packages("flextable") + library(flextable)}
   if(!require(dplyr)){install.packages("dplyr") + library(dplyr)}
   dataframe %>%
@@ -7,5 +7,15 @@ niceTable <- function (dataframe) {
     fontsize(part = "all", size = 12) %>%
     font(part = "all", fontname = "Times New Roman") %>%
     align(align = "center", part = "all") %>%
-    set_table_properties(layout = "autofit")
+    set_table_properties(layout = "autofit") -> table
+  if(special == TRUE) {
+    table %>%
+    italic(j = 3:6, part = "header") %>%
+    compose(i = 1, j = 2, part = "header",
+            value = as_paragraph("β")) %>%
+    compose(i = 1, j = 6, part = "header",
+            value = as_paragraph("sr", as_sup("2"))) %>% 
+    compose(i = 1, j = 5, part = "header",
+            value = as_paragraph("η", as_sub("p"), as_sup("2")))
+    }
 }
