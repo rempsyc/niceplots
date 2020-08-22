123d1b39080b8890595480b452aaf5268c18333d
RemPsyc <RemPsyc@users.noreply.github.com>
2020-08-22 11:25:26 -0400

    Update niceTableFunction.R

--- niceTableFunction.R
+++ niceTableFunction.R
@@ -10,73 +10,81 @@ niceTable <- function (dataframe, italics = NULL, special = FALSE, highlight = F
     set_table_properties(layout = "autofit") -> table
   if(!missing(italics)) {
     table %>%
-    italic(j = italics, part = "header") -> table
+      italic(j = italics, part = "header") -> table
   }
   if("p" %in% names(dataframe)) {
+    format.p <- function(p, precision = 0.001) {
+      digits <- -log(precision, base = 10)
+      p <- formatC(p, format = 'f', digits = digits)
+      p[p == formatC(0, format = 'f', digits = digits)] <- paste0('< ', precision)
+      sub("0", "", p)
+    }
     table %>%
-    italic(j = "p", part = "header") -> table
-    }  
+      italic(j = "p", part = "header") %>%
+      set_formatter(p = function(x)
+        format.p(x)) -> table
+  }  
   if("t" %in% names(dataframe)) {
     table %>%
-    italic(j = "t", part = "header") -> table
-    }
+      italic(j = "t", part = "header") -> table
+  }
   if("SE" %in% names(dataframe)) {
     table %>%
-    italic(j = "SE", part = "header") -> table
-    }
+      italic(j = "SE", part = "header") -> table
+  }
   if("SD" %in% names(dataframe)) {
     table %>%
-    italic(j = "SD", part = "header") -> table
-    }
+      italic(j = "SD", part = "header") -> table
+  }
   if("F" %in% names(dataframe)) {
     table %>%
-    italic(j = "F", part = "header") -> table
-    }
+      italic(j = "F", part = "header") -> table
+  }
   if("df" %in% names(dataframe)) {
     table %>%
-    italic(j = "df", part = "header") -> table
-    }
+      italic(j = "df", part = "header") -> table
+  }
   if("r" %in% names(dataframe)) {
     table %>%
-    italic(j = "r", part = "header") -> table
-    }
-   if("b" %in% names(dataframe)) {
+      italic(j = "r", part = "header") -> table
+  }
+  if("b" %in% names(dataframe)) {
     table %>%
-    italic(j = "b", part = "header") -> table
-    }
+      italic(j = "b", part = "header") -> table
+  }
   if("M" %in% names(dataframe)) {
     table %>%
-    italic(j = "M", part = "header") -> table
-    }
+      italic(j = "M", part = "header") -> table
+  }
   if("B" %in% names(dataframe)) {
     table %>%
-    compose(i = 1, j = "B", part = "header",
-            value = as_paragraph("β")) -> table
-    }
+      compose(i = 1, j = "B", part = "header",
+              value = as_paragraph("β")) -> table
+  }
   if("R2" %in% names(dataframe)) {
     table %>%
-    compose(i = 1, j = "R2", part = "header",
-            value = as_paragraph("R", as_sup("2"))) %>%
-    italic(j = "R2", part = "header") -> table
-    }
+      compose(i = 1, j = "R2", part = "header",
+              value = as_paragraph("R", as_sup("2"))) %>%
+      italic(j = "R2", part = "header") -> table
+  }
   if("sr2" %in% names(dataframe)) {
     table %>%
-    compose(i = 1, j = "sr2", part = "header",
-            value = as_paragraph("sr", as_sup("2"))) %>%
-    italic(j = "sr2", part = "header") -> table
-    }
+      compose(i = 1, j = "sr2", part = "header",
+              value = as_paragraph("sr", as_sup("2"))) %>%
+      italic(j = "sr2", part = "header") -> table
+  }
   if("np2" %in% names(dataframe)) {
     table %>%
-    compose(i = 1, j = "np2", part = "header",
-            value = as_paragraph("η", as_sub("p"), as_sup("2"))) -> table
-    }
+      compose(i = 1, j = "np2", part = "header",
+              value = as_paragraph("η", as_sub("p"), as_sup("2"))) -> table
+  }
   if(highlight == TRUE) {
     table %>%
-    bold(i = ~ signif == TRUE,
-         j = ~ Model + B + t + p + np2 + sr2 + signif) %>%
-    bg(i = ~ signif == TRUE,
-       j = ~ Model + B + t + p + np2 + sr2 + signif,
-       bg = "#CFCAC2") -> table
-    }
+      bold(i = ~ signif == TRUE,
+           j = ~ Model + B + t + p + np2 + sr2 + signif) %>%
+      bg(i = ~ signif == TRUE,
+         j = ~ Model + B + t + p + np2 + sr2 + signif,
+         bg = "#CFCAC2") -> table
+  }
   table
 }
