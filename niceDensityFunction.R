93dd891a43e3127f5d0bde977db19786275b9874
RemPsyc <RemPsyc@users.noreply.github.com>
2021-04-11 10:26:54 -0400

    improved nicedensity to allow ?n groups

--- niceDensityFunction.R
+++ niceDensityFunction.R
@@ -4,43 +4,22 @@ niceDensity <- function(variable, group, data, colours, ytitle="Density", xtitle
   if(!require(ggrepel)){install.packages("ggrepel")}
   library(dplyr)
   library(ggplot2)
-  require(ggrepel)
+  library(ggrepel)
+  options(dplyr.summarise.inform = FALSE)
   data[[group]] <- as.factor(data[[group]])
-  data$variable <- data[,variable]
-  data$group <- factor(data[,group])
-  data[,group] <- factor(data[,group])
-  {if (!missing(groups.labels)) levels(data$group) <- groups.labels}
+  gform <- reformulate(".", response=group)
+  {if (!missing(groups.labels)) levels(data[[group]]) <- groups.labels}
   # Make data for normally distributed lines
-    norm.1 <- data %>%
-    filter(data$group==levels(data$group)[1]) %>%
-    with(data.frame(x = seq(min(variable), 
-                            max(variable), 
-                            length.out=100), 
-                    y = dnorm(seq(min(variable), 
-                                  max(variable), 
-                                  length.out=100), mean(variable), 
-                              sd(variable)))) %>%
-    mutate(group = factor(levels(data$group)[1],levels = levels(data$group)))
-  norm.2 <- data %>%
-    filter(data$group==levels(data$group)[2]) %>%
-    with(data.frame(x = seq(min(variable), 
-                            max(variable), 
-                            length.out=100), 
-                    y = dnorm(seq(min(variable), 
-                                  max(variable), 
-                                  length.out=100), mean(variable), 
-                              sd(variable)))) %>%
-    mutate(group = factor(levels(data$group)[2],levels = levels(data$group)))
-  norm.3 <- data %>%
-    filter(data$group==levels(data$group)[3]) %>%
-    with(data.frame(x = seq(min(variable), 
-                            max(variable), 
-                            length.out=100), 
-                    y = dnorm(seq(min(variable), 
-                                  max(variable), 
-                                  length.out=100), mean(variable), 
-                              sd(variable)))) %>%
-    mutate(group = factor(levels(data$group)[3],levels = levels(data$group)))
+  dat_norm <- data %>% group_by(.data[[group]]) %>% 
+    # HERE IS THE CHANGE: do( 
+    do(summarise(.,x=seq(min(.[[variable]]), 
+                         max(.[[variable]]), 
+                         length.out=100),
+                 y=dnorm(seq(min(.[[variable]]), 
+                             max(.[[variable]]), 
+                             length.out=100), 
+                         mean(.[[variable]]), 
+                         sd(.[[variable]]))))
   # Make data for the Shapiro-Wilk tests
   if (shapiro == TRUE) { 
     format.p <- function(p, precision = 0.001) {
@@ -52,38 +31,19 @@ niceDensity <- function(variable, group, data, colours, ytitle="Density", xtitle
         p = paste0('= ', p, " (Shapiro-Wilk)")    }
       sub("0", "", p)
     }
-  data %>%
-    filter(data$group==levels(data$group)[1]) %>%
-    select(variable) %>%
-    unlist %>%
-    shapiro.test() -> shap
-  shapiro.1 <- sprintf("italic('p')~'%s'", format.p(shap$p.value))
-  data %>%
-    filter(data$group==levels(data$group)[2]) %>%
-    select(variable) %>%
-    unlist %>%
-    shapiro.test() -> shap
-  shapiro.2 <- sprintf("italic('p')~'%s'", format.p(shap$p.value))
-  data %>%
-    filter(data$group==levels(data$group)[3]) %>%
-    select(variable) %>%
-    unlist %>%
-    shapiro.test() -> shap
-  shapiro.3 <- sprintf("italic('p')~'%s'", format.p(shap$p.value))
-  # Make annotations dataframes
-  dat_text <- data.frame(
-    group = levels(data$group),
-    text = c(shapiro.1, shapiro.2, shapiro.3))
+    suppressWarnings(
+      dat_text <- data %>% group_by(.data[[group]]) %>% 
+        summarise(text=shapiro.test(.data[[variable]])$p.value) %>% 
+        mutate(text=sprintf("italic('p')~'%s'", format.p(text)))
+    )
   }
   # Make plot
-  ggplot(data, aes(x=variable, fill=group)) +
+  ggplot(data, aes_string(x=variable, fill=group)) +
     geom_density(alpha=0.6, size=1, colour="gray25") +
-    ggtitle(title) +
     theme_bw(base_size = 24) +
-    facet_grid(group ~ .) +
-    geom_line(data = norm.1, aes(x = x, y = y), color = "darkslateblue", size=1.2, alpha=0.9) +
-    geom_line(data = norm.2, aes(x = x, y = y), color = "darkslateblue", size=1.2, alpha=0.9) +
-    geom_line(data = norm.3, aes(x = x, y = y), color = "darkslateblue", size=1.2, alpha=0.9) +
+    ggtitle(title) +
+    facet_grid(gform) +
+    geom_line(data = dat_norm, aes(x = x, y = y), color = "darkslateblue", size=1.2, alpha=0.9) +
     ylab(ytitle) +
     xlab(xtitle) +
     {if (shapiro == TRUE) geom_text_repel(data = dat_text,
@@ -96,7 +56,7 @@ niceDensity <- function(variable, group, data, colours, ytitle="Density", xtitle
                                           parse = TRUE)} +
     {if (!missing(colours)) scale_fill_manual(values=colours)} +
     {if (grid == FALSE) theme(panel.grid.major=element_blank(),
-                             panel.grid.minor=element_blank())} +
+                              panel.grid.minor=element_blank())} +
     theme(plot.title = element_text(hjust = 0.5),
           legend.position = "none",
           axis.text.x = element_text(colour="black"),
