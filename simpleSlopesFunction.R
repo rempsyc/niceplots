007419b2557fbbfeedb8196a396919cba9759516
RemPsyc <RemPsyc@users.noreply.github.com>
2021-08-10 18:18:19 -0400

    combine highs and lows

--- simpleSlopesFunction.R
+++ simpleSlopesFunction.R
@@ -1,4 +1,4 @@
-simpleSlopes <- function(response, predictor, moderator, data) {
+niceMod <- function(response, predictor, moderator, data) {
   if(!require(bootES)){install.packages("bootES")}
   if(!require(lmSupport)){install.packages("lmSupport")}
   library(bootES)
@@ -19,12 +19,14 @@ simpleSlopes <- function(response, predictor, moderator, data) {
   table.stats
 }
 
-simpleSlopes_lows <- function(response, predictor, moderator, data) {
+simpleSlopes <- function(response, predictor, moderator, data) {
   if(!require(bootES)){install.packages("bootES")}
   if(!require(lmSupport)){install.packages("lmSupport")}
   library(bootES)
   library(lmSupport)
   df <- data
+
+  # Calculate simple slopes for LOWS
   df$lows <- unlist(df[,moderator]+sd(unlist(df[,moderator])))
   formulas <- paste(response, "~", predictor, "* lows")
   models.list <- sapply(formulas, lm, data = df, simplify = FALSE, USE.NAMES = TRUE)
@@ -33,18 +35,11 @@ simpleSlopes_lows <- function(response, predictor, moderator, data) {
   stats.list <- mapply(cbind,sums.list,ES.list,SIMPLIFY=FALSE)
   stats.list <- lapply(stats.list, function(x) x[predictor,])
   table.stats <- do.call(rbind.data.frame, stats.list)
-  predictor.names <- paste0(predictor, " (Low-", moderator, ")")
+  predictor.names <- paste0(predictor, " (LOW-", moderator, ")")
   table.stats <- cbind(response, predictor.names, table.stats)
-  names(table.stats) <- c("Dependent Variable", "Predictor (-1 SD)", "b", "t", "p", "sr2")
-  table.stats
-}
-
-simpleSlopes_highs <- function(response, predictor, moderator, data) {
-  if(!require(bootES)){install.packages("bootES")}
-  if(!require(lmSupport)){install.packages("lmSupport")}
-  library(bootES)
-  library(lmSupport)
-  df <- data
+  names(table.stats) <- c("Dependent Variable", "Predictor (+/-1 SD)", "b", "t", "p", "sr2")
+  
+  # Calculate simple slopes for HIGHS
   df$highs <- unlist(df[,moderator]-sd(unlist(df[,moderator])))
   formulas <- paste(response, "~", predictor, "* highs")
   models.list <- sapply(formulas, lm, data = df, simplify = FALSE, USE.NAMES = TRUE)
@@ -52,9 +47,13 @@ simpleSlopes_highs <- function(response, predictor, moderator, data) {
   ES.list <- lapply(models.list, function(x) {modelEffectSizes(x, Print=FALSE)$Effects[-1,4]})
   stats.list <- mapply(cbind,sums.list,ES.list,SIMPLIFY=FALSE)
   stats.list <- lapply(stats.list, function(x) x[predictor,])
-  table.stats <- do.call(rbind.data.frame, stats.list)
-  predictor.names <- paste0(predictor, " (High-", moderator, ")")
-  table.stats <- cbind(response, predictor.names, table.stats)
-  names(table.stats) <- c("Dependent Variable", "Predictor (+1 SD)", "b", "t", "p", "sr2")
-  table.stats
+  table.stats2 <- do.call(rbind.data.frame, stats.list)
+  predictor.names <- paste0(predictor, " (HIGH-", moderator, ")")
+  table.stats2 <- cbind(response, predictor.names, table.stats2)
+  names(table.stats2) <- c("Dependent Variable", "Predictor (+/-1 SD)", "b", "t", "p", "sr2")
+  
+  # Combine both dataframes for both LOWS and HIGHS
+  new.df <- rbind(table.stats,table.stats2)
+  new.df <- new.df[order(new.df$`Dependent Variable`),]
+  new.df
 }
