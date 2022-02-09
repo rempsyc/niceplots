cbdd47f1277f1686805116ff88cb7b8528036f8e
RemPsyc <RemPsyc@users.noreply.github.com>
2022-02-09 13:54:52 -0500

    Made interpretation helper message optional

--- niceAssumptionsFunction.R
+++ niceAssumptionsFunction.R
@@ -1,4 +1,4 @@
-niceAssumptions <- function(model) {
+niceAssumptions <- function(model, interpretation = TRUE) {
   if(!require(lmtest)){install.packages("lmtest")}
   if(!require(crayon)){install.packages("crayon")}
   library(lmtest)
@@ -10,6 +10,6 @@ niceAssumptions <- function(model) {
   dg <- sum(shapiro < .05, bp < .05, dw < .05)
   df <- data.frame("Model..." = model.name, "Normality (Shapiro-Wilk)..." = shapiro, "Homoscedasticity (Breusch-Pagan)..." = bp, "Autocorrelation of residuals (Durbin-Watson)..." = dw, "Diagnostic..." = dg, check.names = FALSE)
   row.names(df) <- NULL
-  cat(yellow("Interpretation: (p) values < .05 imply assumptions are not respected. \nDiagnostic is how many assumptions are not respected for a given model or variable. \n  \n"))
+  if(interpretation == TRUE) { cat(yellow("Interpretation: (p) values < .05 imply assumptions are not respected. \nDiagnostic is how many assumptions are not respected for a given model or variable. \n  \n")) }
   df
 }
