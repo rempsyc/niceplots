cb8fd635c3890cba6b976a400cb8beb0203581a5
RemPsyc <RemPsyc@users.noreply.github.com>
2022-03-29 17:29:28 -0400

    fixed variable name

--- cormatrix_xl.R
+++ cormatrix_xl.R
@@ -2,28 +2,28 @@ cormatrix_xl <- function(data, filename = "mycormatrix", overwrite = TRUE) {
   if(!require(openxlsx)){install.packages("openxlsx")}
   library(openxlsx)
   
-  my.cox.matrix <- cor(data, use = "na.or.complete")
-  my.cox.matrix <- as.data.frame(my.cox.matrix)
+  my.cor.matrix <- cor(data, use = "na.or.complete")
+  my.cor.matrix <- as.data.frame(my.cor.matrix)
   
-  print(round(my.cox.matrix,2))
+  print(round(my.cor.matrix, 2))
   
   wb <- createWorkbook()
   addWorksheet(wb, "Sheet 1")
-  writeData(wb, sheet = 1, my.cox.matrix, rowNames = TRUE)
+  writeData(wb, sheet = 1, my.cor.matrix, rowNames = TRUE)
   
   my.style <- createStyle(numFmt = "0.00")
   addStyle(wb, 
            sheet = 1, 
            style = my.style, 
-           rows = 1:ncol(my.cox.matrix), 
-           cols = 1:ncol(my.cox.matrix), 
+           rows = 1:ncol(my.cor.matrix), 
+           cols = 1:ncol(my.cor.matrix), 
            gridExpand = TRUE)
   
   diagonalStyle <- createStyle(bgFill = "gray")
   mediumStyle <- createStyle(bgFill = "#FBCAC0") # blue
   largeStyle <- createStyle(bgFill = "#F65534") # red
   
-  all.columns <- 1:(ncol(my.cox.matrix)+1)
+  all.columns <- 1:(ncol(my.cor.matrix)+1)
   
   conditionalFormatting(wb, 
                         "Sheet 1",
